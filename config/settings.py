845410e722f1205ced956bfdb7cb1733ac1065c5
humoyun <uktamovhumoyun046@gmail.com>
2026-06-09 15:45:27 +0500

    STATIC_ROOT sozlamasi qo'shildi

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,8 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/',
+STATIC_ROOT = BASE_DIR / 'staticfiles',
 
 GEMINI_API_KEY = "bu yerga api kaliti qo'yiladi"
 
